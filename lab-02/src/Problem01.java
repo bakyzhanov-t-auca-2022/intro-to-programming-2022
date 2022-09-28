a0cfaca3da381f7dc6e94bb0996866e0a05b3c8b
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-09-28 13:16:09 +0600

    Problem01 in lab-02 Complete

---
+++ lab-02/src/Problem01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problem01{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1st value? ");
+        int a = scanner.nextInt();
+        System.out.print("2nd value? ");
+        int b = scanner.nextInt();
+        System.out.printf("Before swapping: a = %d; b = %d;%n", a, b);
+        int t = a;
+        a = b;
+        b = t;
+        System.out.printf("After swapping: a = %d; b = %d;", a, b);
+    }
+}
