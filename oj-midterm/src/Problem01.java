c59e552bd6d13658ba4f1e98b04db7282bec1b07
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-09-28 14:03:14 +0600

    Problem01 in lab-02 Complete

---
+++ oj-midterm/src/Problem01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String ss = s.nextLine();
+        System.out.printf("%s %s %s", ss, ss ,ss);
+    }
+}
