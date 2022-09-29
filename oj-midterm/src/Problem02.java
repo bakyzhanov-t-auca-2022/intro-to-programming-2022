44154b06b690687c998f3e0a51bae7b5f96190a1
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-09-29 21:32:59 +0600

    Problem02 in oj-midterm Complete

---
+++ oj-midterm/src/Problem02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Problem02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
