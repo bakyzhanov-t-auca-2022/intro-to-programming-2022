1daf0bf26d2bab90cdaaff6674829103f286ff4c
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-10-01 19:33:10 +0600

    Problem03 in oj-midterm Complete (Accepted in Kattis)

---
+++ oj-midterm/src/Problem03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        int m = scanner.nextInt();
+        int o = m + m - y;
+        System.out.println(o);
+    }
+}
