186fc66521c00024ff5207b99877c94019335136
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-10-02 16:35:04 +0600

    Problem08 in lab-02 Complete

---
+++ lab-02/src/Problem08.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number in pounds: ");
+        Scanner scanner = new Scanner(System.in);
+        double p = scanner.nextDouble();
+        double kg = p * 0.454;
+
+        System.out.printf("%.1f pounds is %.3f kilograms",p,kg);
+        //Как показывать double без 0
+    }
+}
