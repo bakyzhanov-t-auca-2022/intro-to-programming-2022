079ab3ba8398391a28927337e0913882c1f69fa4
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-10-17 11:09:38 +0600

    Problem16 in oj-midterm Accepted

---
+++ oj-midterm/src/Problem16.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        if(num1 % num2 == 0) System.out.println("yes");
+        else System.out.println("no");
+    }
+}
