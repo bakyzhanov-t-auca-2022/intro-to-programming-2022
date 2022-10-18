a51066c540f0cef6642d4aa6509b39b0de184489
Temirlan Bakyzhanov <bakyzhanov_t@auca.kg>
2022-10-18 12:07:04 +0600

    Problem25 in oj-midterm Accepted

---
+++ oj-midterm/src/Problem25.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String lol = scanner.next();
+        if (lol.contains("ss")) System.out.println("hiss");
+        else System.out.println("no hiss");
+    }
+}
